fed695422f153925ab7e666b1e3f1289a0ff18eb
Iyad-Charef <oi_charef@esi.dz>
2025-12-24 19:57:09 +0100

    comments

--- functions.h
+++ functions.h
@@ -10,29 +10,34 @@
 #define MAXTAB 10
 #define MAX_KEY_LEN 20
 
+// record struct
 typedef struct {
     char key[MAX_KEY_LEN];
     char name[50];
     int age;
 } t_rec;
 
+//block struct
 typedef struct blck {
     t_rec tab[MAXTAB];
     char del[MAXTAB];
     int nb;
 } t_block;
 
+// file header
 typedef struct hdr {
     long nBlock;
     long nIns;
     long nDel;
 } t_header;
 
+// tnof file struct
 typedef struct TnOFstr {
     FILE *f;
     t_header h;
 } t_TnOF;
 
+//machine abstraite functions
 void TnOF_open(t_TnOF **F, char *fname, char mode);
 void TnOF_close(t_TnOF *F);
 void TnOF_readBlock(t_TnOF *F, long i, t_block *buf);
@@ -42,12 +47,14 @@ long getHeader(t_TnOF *F, char *hname);
 
 extern int K_FRAGMENTS;
 extern int M_BUFFERS;
-
+//TP functions
 int hash_function(char *key, int K);
 void partition_file_by_hashing(char *source_filename, int K, int M);
 int search_in_fragments(char *key, int K, int *fragment_num, long *block_num, int *position, t_rec *record);
 int insert_into_fragments(t_rec record, int K);
 int delete_from_fragments(char *key, int K);
+
+// helper functions
 void get_fragment_filename(int fragment_num, char *filename);
 void display_fragment(int fragment_num);
 void display_all_fragments(int K);
